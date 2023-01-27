95054f8dfd292c0dbda2caf75fad5c7419247eb7
Om <adityajain2002j@gmail.com>
2023-01-27 22:46:21 +0530

    updated sql files

--- user_details.sql
+++ user_details.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2023 at 07:24 PM
+-- Generation Time: Jan 27, 2023 at 06:14 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
